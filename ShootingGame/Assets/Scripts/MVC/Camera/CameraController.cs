c8756f6a60a4bd9cc6ebf432c7fd45a386ec35b8
Алексей Скворцов <sotoder@gmail.com>
2021-10-22 10:39:26 +0300

    3

--- ShootingGame/Assets/Scripts/MVC/Camera/CameraController.cs
+++ ShootingGame/Assets/Scripts/MVC/Camera/CameraController.cs
@@ -68,6 +68,8 @@ namespace Model.ShootingGame
             {
                 CameraFolow(deltaTime, camera, target);
             }
+
+
         }
 
         private void CameraFolow(float deltaTime, Camera camera, Transform target)
